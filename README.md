bcb9b1aa4c5635f0edde5fc06c3070eade48f4c6
Greeley Wentz <jasonhorowitz@Jasons-MacBook-Pro.local>
2019-01-26 16:20:59 -0500

    saturday edit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # liri-node-app
 liri-node-app
+
+# Instructions
+1. Run node liri.js concert-this "artist name" to output the following:
+    * Name of the venue.
+    * Venue location.
+    * Date of the Event.
+2. Run node liri.js spotify-this-song "song name" to output the following:
+    * Artist(s).
+    * The song's name.
+    * A preview link of the song from Spotify.
+    * The album that the song is from.
+3. Run node liri.js movie-this "movie name" to output the following:
+    * Title of the movie.
+    * Year the movie came out.
+    * IMDB Rating of the movie.
+    * Rotten Tomatoes Rating of the movie.
+    * Country where the movie was produced.
+    * Language of the movie.
+    * Plot of the movie.
+    * Actors in the movie.
+4. Run node liri.js do-what-it-says to output the data above based on the string in random.txt
+
+#Requirements:
+require("dotenv")
+require("fs")
+require("axios")
+require("inquirer")
+require("bandsintown")
+require("omdb")
+require("moment")
+require("node-spotify-api")
+require("./keys.js")
+
+#Sample Recording:
+https://www.screencast.com/t/1tiixL2o 
--- liri.js
+++ liri.js
@@ -29,17 +29,24 @@ switch (action) {
         break;
 }
 
-function artist() {
-    var nodeArgs = process.argv;
+function artist(band) {
     var artistName = "";
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            artistName = artistName + "+" + nodeArgs[i];
+    var nodeArgs = process.argv;
+    if (band === undefined) {
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                artistName = artistName + "+" + nodeArgs[i];
+                console.log(artistName);
+            }
+
+            else {
+                artistName += nodeArgs[i];
+            }
         }
-        else {
-            artistName += nodeArgs[i];
-        }
+    } else {
+        artistName = band;
     }
+
     axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=trilogy").then(
         function (response) {
 
@@ -58,54 +65,92 @@ function artist() {
 
 
 
+
 function movie(movies) {
 
-    var nodeArgs = process.argv;
+    // var nodeArgs = process.argv;
+    // var movieName = "";
+    // for (var i = 3; i < nodeArgs.length; i++) {
+    //     if (i > 3 && i < nodeArgs.length) {
+    //         movieName = movieName + "+" + nodeArgs[i];
+    //     }
+    //     else {
+    //         movieName += nodeArgs[i];
+    //     }
+    // }
+
     var movieName = "";
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            movieName = movieName + "+" + nodeArgs[i];
+    var nodeArgs = process.argv;
+    if (movies === undefined) {
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
+                console.log(movieName);
+            }
+            else {
+                movieName += nodeArgs[i];
+                console.log(movieName);
+            }
         }
-        else {
-            movieName += nodeArgs[i];
-        }
+    } else {
+        movieName = movies;
+
     }
-    
 
+    if (movieName === "") {
+        axios.get("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy").then(
+            function (response) {
 
-    axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
-        function (response) {
+                //title of the movie
+                console.log("Title: " + response.data.Title);
+                //year the movie came out
+                console.log("Year: " + response.data.Year);
+                //IMDB rating of the movie
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                //rotten tomatoes rating of the movie
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                //country where the movie was produced
+                console.log("Country: " + response.data.Country);
+                //language of the movie
+                console.log("Language: " + response.data.Language);
+                //plot of the movie
+                console.log("Plot: " + response.data.Plot);
+                //actors in the movie
+                console.log("Actors: " + response.data.Actors);
+            }
 
-
-            //title of the movie
-            console.log("Title: " + response.data.Title);
-            //year the movie came out
-            console.log("Year: " + response.data.Year);
-            //IMDB rating of the movie
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            //rotten tomatoes rating of the movie
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-            //country where the movie was produced
-            console.log("Country: " + response.data.Country);
-            //language of the movie
-            console.log("Language: " + response.data.Language);
-            //plot of the movie
-            console.log("Plot: " + response.data.Plot);
-            //actors in the movie
-            console.log("Actors: " + response.data.Actors);
-        }
-    )
-}
-
-function vote(age){
-    var votingAge = 18;
-    if (age >= votingAge){
-        console.log("You can vote");
+        )
     }
-    else {console.log("You can't vote")
+
+    else {
+        axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+
+                //title of the movie
+                console.log("Title: " + response.data.Title);
+                //year the movie came out
+                console.log("Year: " + response.data.Year);
+                //IMDB rating of the movie
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                //rotten tomatoes rating of the movie
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                //country where the movie was produced
+                console.log("Country: " + response.data.Country);
+                //language of the movie
+                console.log("Language: " + response.data.Language);
+                //plot of the movie
+                console.log("Plot: " + response.data.Plot);
+                //actors in the movie
+                console.log("Actors: " + response.data.Actors);
+            }
+
+        )
+    }
 }
-}
-vote(12)
+
+
+
+
 
 
 function song(songs) {
@@ -114,10 +159,10 @@ function song(songs) {
     var keys = require("./keys.js")
     var spotify = new Spotify(keys.spotify);
     var name;
-    if (songs === undefined){
+    if (songs === undefined) {
         name = process.argv[3]
     }
-    else {name = songs};
+    else { name = songs };
 
 
 
@@ -133,7 +178,7 @@ function song(songs) {
     //     }
     // }
 
-    spotify.search({ type: 'track', query: name, limit: 1 }, function (err, data) {
+    spotify.search({ type: 'track', query: name || "Ace-of-Base", limit: 1 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
@@ -155,27 +200,44 @@ function song(songs) {
 }
 
 
+
 function says() {
-// Includes the FS package for reading and writing packages
-var fs = require("fs");
+    // Includes the FS package for reading and writing packages
+    var fs = require("fs");
 
-// Running the readFile module that's inside of fs.
-// Stores the read information into the variable "data"
-fs.readFile("random.txt", "utf8", function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
+    // Running the readFile module that's inside of fs.
+    // Stores the read information into the variable "data"
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data);
 
-    var output = data.split(",");
-    var songs = output[1];
-    var actions = output[0];
-    console.log(actions)
-    song(songs);
-    
-    
+        var output = data.split(",");
+        // var songs = output[1];
+        // var band = output[1];
+        var actions = output[0];
 
 
-   
-});
+        if (actions === "spotify-this-song") {
+            var songs = output[1].trim();//trim to remove white spaces
+            song(songs);
+        }
+        if (actions === "movie-this") {
+            var movies = output[1].trim();
+            //console.log(movies)
+            movie(movies);
+        }
+
+
+        if (actions === "concert-this") {
+            var band = output[1].trim();
+            artist(band);
+        }
+
+
+
+
+
+    });
 }
